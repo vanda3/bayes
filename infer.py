63109cb058d8a003e66bdc6e9f0c77bae3074ad5
Vanda Azevedo <vanda.fba@gmail.com>
2019-04-02 07:30:36 +0100

    update

---
+++ infer.py
@@ -0,0 +1,61 @@
+from parser import parser, debug
+
+def inference(nodes, q, es, debug):
+    ancestor=[]
+    e=[]
+    ancestor.append(q)
+    evl=[] # list of ancestors
+    for k,v in es.items():
+        ancestor.append(k)
+        e.appedn(k)
+    i=0
+    # find all ancestors
+    while len(ancestor)!=0:
+        curr=ancestor.pop(0)
+        if debug:
+            print("CURR: ",curr)
+        if curr not in evl:
+            if debug:
+                print("Iter ",i,": ",ancestor)
+            evl.append(curr)
+            parents=nodes[curr].parent
+            if len(parents)!=0:
+                for p in parents:
+                    if p not in evl: 
+                        ancestor.append(p)
+            if debug:
+                print("ANC: ", ancestor)
+                print("EVL: ", evl)
+                i+=1
+    for name, node in nodes.items():
+        if name==q or name in e:
+            enum()
+    
+def enum(X, e, es, nodes):
+    dist=[]
+    for name, node in nodes.items():
+        if name==X or name in e:
+
+
+
+if __name__ == "__main__":
+    file=""
+    es={}
+    print("Input BIF file name w/o extension")
+    file=str(input())
+    nodes=parser(file)
+    debug(nodes)
+    print("Input query variable")
+    q=str(input())
+    print("Input evidence (e.g. X=false), type # when finished")
+    e=str(input())
+    print("evidence: ",e)
+    while e.count('#')==0:
+        a=e.split('=')
+        if(a[1] in nodes[a[0]].classes):
+            es[a[0].strip()]=a[1].strip()
+            print("0:",a[0].strip(), ", 1:",a[1].strip())
+        else:
+            print("Error, available values:", nodes[a[0]].classes)
+        e=str(input())
+    inference(nodes,q,es,True)
---
+++ parser.py
@@ -0,0 +1,99 @@
+import math 
+import re
+import numpy as np
+
+
+class Node:
+    def __init__(self, name):
+        self.name=name
+        self.classes=[]
+        self.parent=[]
+        self.child=[]
+        self.probs={}
+    def addProb(self, values, prob):
+        self.probs[repr(values)]=prob
+
+
+def debug(nodes):
+    for key, n in nodes.items():
+        print("Name: ",n.name)
+        print("Classes: ",n.classes)
+        print("Parents: ",n.parent)
+        print("Children: ",n.child)
+        print("Probabilities: ")
+        for key, value in n.probs.items():
+            print("\t",key," ", value)
+
+def parser(name):
+    char_list = '[(),;]'
+    nodes = {}
+    bif = []
+    name=str(name)+".bif"
+    file = open(name,mode='r+')
+    for line in file:
+        bif.append(line)
+    for i in range(0, len(bif)):
+        line = [x for x in bif[i].split(' ') if x!= '']
+        fst=line[0].strip()
+        if fst=="variable":
+            name=line[1].strip()
+            nodes[name]=Node(name)
+            i+=1
+            line = [x for x in bif[i].split(' ') if x!= '']
+            fst=line[0].strip()
+            while fst!="}":
+                if fst=="type":
+                    n=int(line[3].strip())
+                    k=0
+                    while n>0:
+                        nodes[name].classes.append(re.sub(char_list, '',line[6+k].strip()))
+                        k+=1
+                        n-=1
+                elif fst=="property":
+                    # DEPOIS
+                    pass
+                else:
+                    pass 
+                i+=1
+                line = [x for x in bif[i].split(' ') if x!= '']
+                fst=line[0].strip()
+        elif fst=="probability":
+            name = line[2].strip()
+            # tem parents
+            if (line[3].strip()=='|'):
+                occur=bif[i].count(',')
+                k=0
+                while k<=occur:
+                    parent=re.sub(char_list, '', line[4+k].strip())
+                    nodes[name].parent.append(parent)
+                    nodes[parent].child.append(name)
+                    k+=1
+                k=0
+                i+=1
+                line = [x for x in bif[i].split(' ') if x!= '']
+                fst=line[0].strip()
+                while fst!="}":
+                    values={}
+                    probs=[]
+                    n_parents=len(nodes[name].parent)
+                    n_classes=len(nodes[name].classes)
+                    for m in range(0,n_parents):
+                        values[nodes[name].parent[m]]=re.sub(char_list, '', line[m].strip())
+                    for j in range(n_parents,n_parents+n_classes):
+                        probs.append(float(re.sub(char_list, '', line[j].strip())))
+                    nodes[name].addProb(values,probs)
+                    i+=1
+                    line = [x for x in bif[i].split(' ') if x!= '']
+                    fst=line[0].strip()
+            else:
+                i+=1
+                line = [x for x in bif[i].split(' ') if x!= '']
+                fst=line[0].strip()
+                probs=[]
+                n_classes=len(nodes[name].classes)
+                for j in range(1,n_classes+1):
+                    probs.append(float(re.sub(char_list, "", line[j].strip())))
+                nodes[name].addProb([],probs)
+        else: #network
+            pass
+    return nodes
