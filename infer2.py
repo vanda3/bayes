2d844754ee36cbd6b02458589daec3d6842759e3
MarioDSB <up201504054@fc.up.pt>
2019-04-25 22:29:44 +0100

    Printed all the probabilities

--- infer2.py
+++ infer2.py
@@ -3,36 +3,37 @@ from copy import deepcopy
 from ast import literal_eval
 import time
 
-space=0
-deb=True
+space = 0
+deb = True
+
 
 # Core Algorithm
-def core(nodes, names, q, e):
+def core(nodes, q, e):
     global space
-    space=0
+    space = 0
     start = time.time()
     result = init_factors(nodes, q, e, 1)
-    end= time.time()
-    print("\nTop-Down strategy:\n",result)
-    print("Time elapsed=",end-start," Space used=",space)
+    end = time.time()
+    print("\nTop-Down strategy:\n", result)
+    print("Time elapsed=", end - start, " Space used=", space)
     print("--------------------------------------------------")
-    space=0
+    space = 0
     start2 = time.time()
     result2 = init_factors(nodes, q, e, 2)
-    end2= time.time()
-    print("\nBottom-Up strategy:\n",result2)
-    print("Time elapsed =",end2-start2," Space used=",space)
+    end2 = time.time()
+    print("\nBottom-Up strategy:\n", result2)
+    print("Time elapsed =", end2 - start2, " Space used=", space)
     print("--------------------------------------------------")
-    space=0
+    space = 0
     start3 = time.time()
     result3 = init_factors(nodes, q, e, 3)
-    end3= time.time()
-    print("\nMost Cardinal strategy:\n",result3)
-    print("Time elapsed =",end3-start3," Space used=",space)
+    end3 = time.time()
+    print("\nMost Cardinal strategy:\n", result3)
+    print("Time elapsed =", end3 - start3, " Space used=", space)
     print("--------------------------------------------------")
 
 
-def gen_perms(vars,nodes):
+def gen_perms(vars, nodes):
     """
     Generates all possible permutations of the given variables
     :param vars:        The variables to permutate
@@ -77,18 +78,19 @@ def process_query(nodes, var, perm):
         return nodes[var].probs['{}'][0]
     # The variable has parents. Query its probability
     else:
-        return nodes[var].getProbability(perm)      # Get the probability
+        return nodes[var].getProbability(perm)  # Get the probability
+
 
 # Generates valid permutations of values for factors
 def make_factor(nodes, var, factor_vars, e):
     global space
-    space+=1
+    space += 1
     factor_vars.sort()
 
     all_vars = deepcopy(nodes[var].parent)
     all_vars.append(var)
 
-    perms = gen_perms(all_vars,nodes)
+    perms = gen_perms(all_vars, nodes)
 
     # Filter out permutations not in accord with the evidence
     perms = filter_perms(perms, e)
@@ -98,10 +100,11 @@ def make_factor(nodes, var, factor_vars, e):
     for perm in perms:
         prob = process_query(nodes, var, perm)
         probabilities[perm] = prob
-        
+
     return factor_vars, probabilities
 
-# Removes unecessary values from table
+
+# Removes unnecessary values from table
 def filter_perms(perms, e):
     old_perms = deepcopy(perms)
     for perm in old_perms:
@@ -118,7 +121,7 @@ def filter_perms(perms, e):
 
 def product(nodes, factorA, factorB, e):
     global space
-    space+=1
+    space += 1
     vars = []
     vars.extend(factorA[0])
     vars.extend(factorB[0])
@@ -127,7 +130,7 @@ def product(nodes, factorA, factorB, e):
 
     # Eliminate repeated variables
     vars = list(set(vars))
-    perms = gen_perms(vars,nodes)
+    perms = gen_perms(vars, nodes)
 
     # Remove the perms that don't match the evidence
     perms = filter_perms(perms, e)
@@ -213,7 +216,7 @@ def sum_out(nodes, var, factors, e):
         for j, v in enumerate(factor[0]):
             if v == var:
                 # variables of the new factor (remove var from the current factor)
-                new_vars = factor[0][:j] + factor[0][j+1:]
+                new_vars = factor[0][:j] + factor[0][j + 1:]
                 var_probs = {}
                 for tup, prob in factor[1].items():
                     perm = tuple_to_dict(tup)
@@ -239,6 +242,7 @@ def sum_out(nodes, var, factors, e):
                     del factors[i]
     return factors
 
+
 def init_factors(nodes, q, e, flag):
     global deb
     elimd = set()
@@ -247,16 +251,16 @@ def init_factors(nodes, q, e, flag):
     i = 0
     while i < len(nodes):
         vars = filter(lambda var: var not in elimd, list(nodes.keys()))
-        if flag==1:
+        if flag == 1:
             vars = filter(lambda var: all(parent in elimd for parent in nodes[var].parent), vars)
-        elif flag==2:
+        elif flag == 2:
             vars = filter(lambda var: all(child in elimd for child in nodes[var].child), vars)
         else:
-            tmp={}
+            tmp = {}
             for v in vars:
-                tmp[v]=nodes[v].cardinal
-            tmp=sorted(tmp.items(), key=lambda x: x[1],reverse=True)
-            vars=[]
+                tmp[v] = nodes[v].cardinal
+            tmp = sorted(tmp.items(), key=lambda x: x[1], reverse=True)
+            vars = []
             for a in tmp:
                 vars.append(a[0])
         factor_vars = {}
@@ -294,20 +298,20 @@ def init_factors(nodes, q, e, flag):
 def normalize_factor(nodes, factor, q):
     sum_probs = sum(factor[1].values())
     probs = []
-    probs_dict={}
+    probs_dict = {}
     for tup, prob in factor[1].items():
         dit = tuple_to_dict(tup)
         for clas in nodes[q].classes:
             if dit[q] == clas:
                 if clas in probs_dict:
-                    probs_dict[clas]+=prob
+                    probs_dict[clas] += prob
                 else:
-                    probs_dict[clas]=prob
+                    probs_dict[clas] = prob
     for clas, prob in probs_dict.items():
-        if len(probs)==0:
-            probs=[(clas,round(prob/sum_probs,5))]
+        if len(probs) == 0:
+            probs = [(clas, round(prob / sum_probs, 5))]
         else:
-            probs.append((clas,round(prob/sum_probs,5)))
+            probs.append((clas, round(prob / sum_probs, 5)))
     return probs
 
 
@@ -321,7 +325,7 @@ if __name__ == "__main__":
     print("Available Variables: ", names)
     print("Pr? ", end='')
     query = str(input())
-    q, e, f = queryParser(query,nodes)
+    q, e, f = queryParser(query, nodes)
     while True:
         if q not in names:
             print("Error. ", q, " not available.")
@@ -335,8 +339,8 @@ if __name__ == "__main__":
                     f = -1
                 elif e[ev] not in nodes[ev].classes:
                     print("Error. '", e[ev], "' isn't valid.")
-                    print("Available values for variable ",ev,": ",nodes[ev].classes,".")
-                    f= -1
+                    print("Available values for variable ", ev, ": ", nodes[ev].classes, ".")
+                    f = -1
                 else:
                     pass
         if f == -1:
@@ -346,5 +350,5 @@ if __name__ == "__main__":
         else:
             break
     print("--------------------------------------------------")
-    print("Tamanho da rede: ",len(nodes))
-    core(nodes, names, q, e)
+    print("Tamanho da rede: ", len(nodes))
+    core(nodes, q, e)
